24f94a68bf40f04936e5968ef7e51d9863bc67f2
C_pumpkin <62928527+1911086027@users.noreply.github.com>
2024-05-02 22:42:46 +0800

    renew

---
+++ configs/diy-part2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Add password
+sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.128.3/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+sed -i 's/OpenWrt/Pumpkin-Router/g' package/base-files/files/bin/config_generate
+
+# Add packges
+rm -rf package/luci-app-amlogic
+git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+rm -rf package/luci-app-poweroff
+git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
